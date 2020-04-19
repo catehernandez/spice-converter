a81dac4d62ec6337a057728bac790443f740974e
Cate <ceh2181@columbia.edu>
2020-04-19 10:18:01 -0400

    Basic select form in place

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: 'allspice' };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+      <select value={this.state.value} onChange={this.handleChange}>
+        <option value="allspice">berries</option>
+        <option value="black pepper">peppercorns</option>
+        <option value="cardamom">pods</option>
+      </select>
+    );
+  }
 }
 
 export default App;
