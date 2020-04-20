ff2777cc3e117f0eff61b8cc6082589b29550ee8
Cate <ceh2181@columbia.edu>
2020-04-19 21:09:42 -0400

    Update state values

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { value: 'allspice' };
+    this.state = {
+      selectedSpice: '',
+      conversionRatio: 0,
+    };
   }
 
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
+  selectSpice = (event) => {
+    const conversionRatios = {
+      allspice: 1,
+      blackPepper: 1 / 1.5,
+      cardamom: 2,
+    };
+
+    this.setState({
+      selectedSpice: event.target.value,
+      conversionRatio: conversionRatios[event.target.value],
+    });
+
+    console.log('spice', this.state.selectedSpice);
+    console.log('ratio', this.state.conversionRatio);
   };
 
   render() {
     return (
-      <select value={this.state.value} onChange={this.handleChange}>
+      <select value={this.state.selectedSpice} onChange={this.selectSpice}>
         <option value="allspice">berries</option>
-        <option value="black pepper">peppercorns</option>
+        <option value="blackPepper">peppercorns</option>
         <option value="cardamom">pods</option>
       </select>
     );
