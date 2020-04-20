1162f077e658a5e14b7230e81a28928a7fd75056
Cate <ceh2181@columbia.edu>
2020-04-20 12:04:22 -0400

    UI: Add custom select dropdown arrow

--- src/components/StyledSelect.js
+++ src/components/StyledSelect.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import downArrow from '../assets/down-arrow.svg';
 
 const StyledSelect = styled.select`
   appearance: none;
-  background-color: transparent;
+  background: url(${downArrow}) no-repeat right transparent;
   border: none;
   font: inherit;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default StyledSelect;
