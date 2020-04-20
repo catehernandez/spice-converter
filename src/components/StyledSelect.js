0308320803248bd877c68ee469d1129d98e7cd1b
Cate <ceh2181@columbia.edu>
2020-04-20 15:31:16 -0400

    Configure to support absolute path imports from src

--- src/components/StyledSelect.js
+++ src/components/StyledSelect.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import downArrow from '../assets/down-arrow.svg';
+import downArrow from 'assets/down-arrow.svg';
 
 const StyledSelect = styled.select`
   appearance: none;
