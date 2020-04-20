3ad64b10d0f5aaf0e4881c8c4a88f367f81b7c76
Cate <ceh2181@columbia.edu>
2020-04-20 13:55:47 -0400

    UI: Improve select arrow styling
    - Mute arrow fill slightly
    - Size arrow slightly smaller than font size

--- src/components/StyledSelect.js
+++ src/components/StyledSelect.js
@@ -4,6 +4,7 @@ import downArrow from '../assets/down-arrow.svg';
 const StyledSelect = styled.select`
   appearance: none;
   background: url(${downArrow}) no-repeat right transparent;
+  background-size: auto 14px;
   border: none;
   font: inherit;
   width: min-content;
