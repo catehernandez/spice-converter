39b87d29a86995a2c0ade58c51ef7e8548c75cfa
Cate <ceh2181@columbia.edu>
2020-04-22 15:35:44 -0400

    Misc. cleanup

--- src/components/SpiceConverter/index.js
+++ src/components/SpiceConverter/index.js
@@ -4,7 +4,7 @@ import customSelectStyles from 'shared/CustomSelectStyles';
 
 import { Spices, WholeUnits, ConversionRatios } from './SpiceConfig';
 import { NumericalInput } from 'shared/StyledInput';
-import EqualSign from 'shared/EqualSign';
+import EqualSign from './EqualSign';
 
 class App extends React.Component {
   constructor(props) {
@@ -54,29 +54,25 @@ class App extends React.Component {
 
     return (
       <React.Fragment>
-        <div>
-          <NumericalInput
-            type="number"
-            onChange={this.convertToGround}
-            value={this.state.wholeUnits}
-            step="0.25"
-          />
-          <br />
-          <Select
-            styles={customSelectStyles}
-            onChange={this.selectSpice}
-            options={options}
-          ></Select>
-        </div>
+        <NumericalInput
+          type="number"
+          onChange={this.convertToGround}
+          value={this.state.wholeUnits}
+          step="0.25"
+        />
+        <br />
+        <Select
+          styles={customSelectStyles}
+          onChange={this.selectSpice}
+          options={options}
+        ></Select>
         <EqualSign />
-        <div>
-          <NumericalInput
-            type="number"
-            onChange={this.convertToWhole}
-            value={this.state.groundUnits}
-            step="0.25"
-          />
-        </div>
+        <NumericalInput
+          type="number"
+          onChange={this.convertToWhole}
+          value={this.state.groundUnits}
+          step="0.25"
+        />
         <div>tsp ground {this.state.selectedSpice}</div>
       </React.Fragment>
     );
